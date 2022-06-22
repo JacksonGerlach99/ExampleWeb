0ea6b9894d1cf6a61cdeb6cc3e83fac846185dd3
JacksonGerlach99 <59943861+JacksonGerlach99@users.noreply.github.com>
2022-06-21 23:24:30 -0700

    Add files via upload

--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -1,84 +1,41 @@
 import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei"
-import { useFrame } from "@react-three/fiber"
-import { useEffect, useRef } from "react"
+import { useFrame, useLoader } from "@react-three/fiber"
+import { useEffect, useRef, useMemo } from "react"
 import { Camera } from "three"
 import {angleToRadians} from "../../Assets/angle"
+import circleImg from '../../Assets/circle.png';
 import * as THREE from "three";
 import gsap from "gsap"
+import { BufferAttribute } from "three";
 
-function Three() {
+function Three({ count = 1000 }) {
     const orbitControlsRef = useRef(null);
-    useFrame((state) =>{
-        if(!!orbitControlsRef) {
-            const {x,y} =state.mouse;
-            orbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(45));
-            orbitControlsRef.current.setPolarAngle((y+1) * angleToRadians(90-30));
-            orbitControlsRef.current.update();
-        }
 
-    })
-
-    //animation
-    const ballRef = useRef(null);
-    useEffect(() =>{
-        if(!!ballRef.current){
-
-            //timeline
-            const timeline = gsap.timeline({paused:true});
-
-            //x-axis motion 
-            timeline.to(ballRef.current.position, {
-                x: 1,
-                duration: 2,
-            })
-
-            //y-axis motion
-            timeline.to(ballRef.current.position, {
-                y:0.5,
-                duration:1,
-                ease: "bounce.out"
-            },"<")
-            
-            timeline.play();
-
-        }
-
-    }, [ballRef.current])
+    const imgTex = useLoader(THREE.TextureLoader, circleImg);
+    const bufferRef = useRef();
+    
+    const points = useMemo(() => {
+        const p = new Array(count).fill(0).map((v) => (0.5 - Math.random()) * 7.5);
+        return new BufferAttribute(new Float32Array(p), 3);
+      }, [count]);
 
     
     return(
         <>
         <PerspectiveCamera makeDefault position={[0,1,5]}/>
         <OrbitControls ref={orbitControlsRef} enableZoom={false} minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(80)}/>
-
-
-
-        {/* Ball */}
-        <mesh position={[-2, 1.5, 0]} castShadow ref={ballRef}>
-            <sphereGeometry args={[0.5,32,32]}/>
-            <meshStandardMaterial color="#FFFFFF" metalness={0.6} roughness={0.2}/>
-        </mesh>
-        {/* floor */}
-        <mesh rotation={[-(angleToRadians(90)),0,0]} receiveShadow>
-            <planeGeometry args={[20,20]}/>
-            <meshStandardMaterial color="#4EC5F1"/>
-        </mesh>
-
-
-
-        <ambientLight args={["#FFFFFF", 0.25]}/>
-
-
-        {/* spotlight light */}
-        <spotLight args={ ["#ffffff", 1.5, 7, angleToRadians(45), 0.4 ]} position={[-3, 1, 0]} castShadow/>
-        
-        {/* environment */}
-        <Environment background>
-            <mesh>
-                <sphereGeometry args={[50, 100,100]} />
-                <meshBasicMaterial color={"#11a0d4"} side = {THREE.BackSide}/>
-            </mesh>
-        </Environment>
+        <points>
+            <bufferGeometry>
+                <bufferAttribute attach={"attributes-position"} {...points} />
+            </bufferGeometry>
+            <pointsMaterial
+                map={imgTex}
+                size={0.1}
+                threshold={0.1}
+                color={0xff00ff}
+                sizeAttenuation={true}
+            />
+        </points>
 
         </>
         
